1ae23907b5662c8e257c4d8d6ccf26ac4b877a1f
Ben Gates <benjamin_gates@outlook.com>
2021-07-26 23:55:01 -0600

    first commit for backend

---
+++ backend/src/app.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
+const express = require("express");
+const app = express();
+
+module.exports = app;
